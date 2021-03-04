54595c4100bf0b3dcf6d7ef34650d6a6440293f2
amaliegrignon <79875580+amaliegrignon@users.noreply.github.com>
2021-03-04 11:35:19 +0100

    Add files via upload

---
+++ ChatRoom.cs
@@ -0,0 +1,10 @@
+namespace DIKULecture {
+    public class ChatRoom {
+        public string name {get; private set;} 
+        public ChatRoom(string name) {
+            this.name = name;
+        }
+    }
+}
+
+
